7c412787b8b5a81f03916a3972c7ffce98a75912
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2024-08-07 13:10:08 +0200

    Add workaround for kind-projector

--- project/Publish.scala
+++ project/Publish.scala
@@ -8,5 +8,14 @@ object Publish extends AutoPlugin {
   override def requires = SpiewakSonatypePlugin
   override def trigger = allRequirements
 
-  override def projectSettings = Seq(sonatypeCredentialHost := "s01.oss.sonatype.org")
+  override def projectSettings = Seq(
+    sonatypeCredentialHost := "s01.oss.sonatype.org",
+    // Workaround until sbt-spiewak-sonatype updates kind-projector
+    // https://github.com/djspiewak/sbt-spiewak/pull/131
+    libraryDependencies ~= {
+      _.map { dep =>
+        if (dep.name == "kind-projector") dep.withRevision("0.13.3")
+        else dep
+      }
+    })
 }
